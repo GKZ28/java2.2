7d18d0ed5ccac82da14379b743101de08451db73
gkz <giorgadzekonstantin@gmail.com>
2023-06-21 15:07:38 +0400

    Сorrected the calculation

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 100;//сумма на счету
-        int addAmount = 900;//сумма пополнения
-        int totalAmount = amount + addAmount;//итоговая сумма на счету
+        int addAmount = 1000;//сумма пополнения
         int bonus = addAmount / 100;
-        System.out.println("Сумма на счету:");
-        System.out.println(totalAmount);
+        int totalAmount = amount + addAmount;//итоговая сумма на счету
+
 
         if (addAmount >= 1000) {
+            System.out.println("Сумма на счету:");
+            System.out.println(totalAmount + bonus);
             System.out.println("Количество бонусов:");
             System.out.println(bonus);
         } else {
+            System.out.println("Сумма на счету:");
+            System.out.println(totalAmount);
             System.out.println("Количество бонусов:");
             System.out.println("0");
         }
